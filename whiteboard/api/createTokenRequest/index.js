f4538a28f7e4ad70414e25278843693a5e116b4c
Sissy2222 <98468165+Sissy2222@users.noreply.github.com>
2024-09-10 04:09:58 -0500

    Update index.js

--- whiteboard/api/createTokenRequest/index.js
+++ whiteboard/api/createTokenRequest/index.js
@@ -1,7 +1,7 @@
 const Ably = require('ably/promises');
 
 module.exports = async function (context, req) {
-    const client = new Ably.Realtime(process.env.ABLY_API_KEY);
+    const client = new Ably.Realtime(process.env.FZr9XA.CvsK0Q:Xce_um2jsT5OcTw5bz7szIfM2EwG1otn5diBx8cnHZA);
     const tokenRequestData = await client.auth.createTokenRequest({ clientId: 'ably-whiteboard' });
     context.res = {
         headers: { "content-type": "application/json" },
